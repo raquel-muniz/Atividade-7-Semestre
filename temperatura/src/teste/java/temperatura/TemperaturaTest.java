00c7a48941dac45c7aa638f7e6e53e7942f6f192
RAQUEL CLEMENTE MUNIZ <raquel.muniz@a.ucb.br>
2026-03-25 11:00:01 -0300

    Ajustes e Organização

---
+++ temperatura/src/teste/java/temperatura/TemperaturaTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TemperaturaTest {
+
+    @Test
+    public void testeConversaoCorreta() {
+        double resultado = Temperatura.celsiusParaFahrenheit(0);
+        assertEquals(32, resultado);
+    }
+
+    // Teste propositalmente errado (vai falhar)
+    @Test
+    public void testeFalhaProposital() {
+        double resultado = Temperatura.celsiusParaFahrenheit(10);
+        assertEquals(100, resultado); // valor errado de propósito
+    }
+}
